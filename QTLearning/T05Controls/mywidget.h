7bc88f2e1ddba3d2ab64ab13a05110cae070af3d
Lihit <wenshaoguo0611@gmail.com>
2018-01-10 23:17:47 +0800

    new notes

---
+++ QTLearning/T05Controls/mywidget.h
@@ -0,0 +1,19 @@
+#ifndef MYWIDGET_H
+#define MYWIDGET_H
+
+#include <QWidget>
+#include <QTextEdit>
+class MyWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MyWidget(QWidget *parent = 0);
+    QTextEdit* _edit;
+signals:
+
+public slots:
+    void slotComboBoxIndexChanged(const QString&);
+
+};
+
+#endif // MYWIDGET_H
